dfa76f8b9935e07d63f4488d4dbd4e7c1fd8db7e
Maksim Malofeev <malofee@yandex.ru>
2022-09-11 19:58:45 +0300

    7модуль 1,2,3 задачи

--- 7-module/1-task/index.js
+++ 7-module/1-task/index.js
@@ -1,7 +1,79 @@
 import createElement from '../../assets/lib/create-element.js';
 
 export default class RibbonMenu {
+
+  ribbon = createElement(`<div class="ribbon">
+  </div>`)
+  button_left = createElement(`<button class="ribbon__arrow ribbon__arrow_left ribbon__arrow_visible">
+  <img src="/assets/images/icons/angle-icon.svg" alt="icon">
+</button>`)
+   inner = createElement(`<nav class="ribbon__inner">
+   </nav>`)
+   button_right= createElement(`<button class="ribbon__arrow ribbon__arrow_right">
+   <img src="/assets/images/icons/angle-icon.svg" alt="icon">
+ </button>`)
+   elem = this.ribbon
+
+
   constructor(categories) {
-    this.categories = categories;
+    this.categories = categories
+    this.render()
+    this.addEventListener()
   }
+
+
+  render () {
+    this.ribbon.append(this.button_left,this.inner,this.button_right)
+    this.categories.map((item,index)=>{
+      let a = createElement(`<a href="#" class="ribbon__item" data-id=""></a>`)
+      a.dataset.id = item.id
+      a.innerHTML = item.name
+      index == 0 ? a.className = `ribbon__item ribbon__item_active`:false
+      this.inner.append(a)
+    })
+    this.tittles = Array.from(this.inner.children)  
+    this.button_left.classList.remove(`ribbon__arrow_visible`)
+    this.button_right.classList.add(`ribbon__arrow_visible`)
+  }
+
+  addEventListener () {
+    this.inner.addEventListener(`scroll`,()=>{
+      let scrollWidth= this.inner.scrollWidth
+      let scrollLeft = this.inner.scrollLeft
+      let clientWidth = this.inner.clientWidth
+      let scrollRight = scrollWidth - scrollLeft - clientWidth
+      scrollLeft != 0 ? this.button_left.classList.add(`ribbon__arrow_visible`) :
+      scrollLeft == 0 ? this.button_left.classList.remove(`ribbon__arrow_visible`) 
+      && this.button_right.classList.add(`ribbon__arrow_visible`) : false
+
+      scrollRight < 1 ? this.button_right.classList.remove(`ribbon__arrow_visible`):false
+    })
+    
+
+    this.ribbon.addEventListener(`click`,(e)=>{
+    e.target.closest(`.ribbon__arrow_left`) ? this.inner.scrollBy(-350,0) : false
+    e.target.closest(`.ribbon__arrow_right`) ? this.inner.scrollBy(350,0) : false
+    
+    if (e.target.closest(`.ribbon__item`)) {
+      e.preventDefault()
+      this.tittles.forEach(elem => {elem.classList.remove(`ribbon__item_active`)})
+      e.target.classList.add(`ribbon__item_active`)
+      this.ribbon.dispatchEvent(new CustomEvent(`ribbon-select`,{
+        detail: e.target.dataset.id,
+        bubbles: true
+      }))
+    }
+
+    })
+
+  }
+
+
+
+
+
+
+  
 }
+
+
--- 7-module/3-task/index.js
+++ 7-module/3-task/index.js
@@ -1,4 +1,69 @@
+import createElement from '../../assets/lib/create-element.js';
 export default class StepSlider {
+
+
   constructor({ steps, value = 0 }) {
+    this.categoties = steps
+    this.render()
+    this.addEventListener()
   }
+
+
+  render () {
+   this.elem = createElement(`<div class="slider">
+   <div class="slider__thumb" style="left: 50%;">
+     <span class="slider__value">2</span>
+   </div>
+   <div class="slider__progress" style="width: 50%;"></div>
+   <div class="slider__steps">
+   </div>
+ </div>`)
+
+   for (let i = 0;i<this.categoties;i++){
+    let span = createElement(`<span></span>`)
+    i==0 ? span.classList.add(`slider__step-active`) : false
+    this.elem.querySelector(`.slider__steps`).append(span)
+   }
+
+   this.step_Percent = 100/(this.categoties-1)
+   this.spans_array = Array.from(this.elem.querySelector(`.slider__steps`).children)
+
+  }
+  
+
+  addEventListener () {
+
+  this.elem.addEventListener(`click`,(e)=>{
+
+    let coordinate = e.clientX 
+    let paddingLeft = this.elem.getBoundingClientRect().left
+    let sliderWidth = this.elem.offsetWidth
+    let relative_coordinate = Math.round((coordinate-paddingLeft)/sliderWidth*(this.categoties-1))
+
+    this.spans_array.forEach(item=>{item.classList.remove(`slider__step-active`)})
+    let number = relative_coordinate
+    let thumb = this.elem.querySelector('.slider__thumb');
+    let progress = this.elem.querySelector('.slider__progress');
+    let leftPercents = number*this.step_Percent;
+    this.elem.querySelector(`.slider__value`).innerHTML = number
+
+    thumb.style.left = `${leftPercents}%`;
+    progress.style.width = `${leftPercents}%`;
+    
+    e.target.classList.add(`slider__step-active`)
+    
+    this.elem.dispatchEvent(new CustomEvent(`slider-change`,{
+      detail: number,
+      bubbles:true
+    }))
+
+  })
+
+
+  }
+
 }
+
+
+
+  
